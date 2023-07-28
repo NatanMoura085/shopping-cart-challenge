4a05e731938c00226061c4880c42f90ff0ac9ec8
Natan <mouranatan933@gmail.com>
2023-07-28 15:16:11 -0300

    feat: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,17 @@
-export default function Header(){
+import React from "react";
+import { ShoppingCartOutline, ShoppingBagOutline } from "@graywolfai/react-heroicons";
 
-    return(
-        <header className="bg-[#ef3c44] p-6">
-dd
-        </header>
-    )
+export default function Header() {
+  return (
+    <header className="bg-[#ef3c44] p-4 flex justify-between items-center">
+      <div className="flex flex-row gap-2 items-center">
+        <ShoppingCartOutline className="h-7 text-cyan-50" />
+        <p className="font-semibold text-xl text-cyan-50">Shopping cart</p>
+      </div>
+
+      <div className="flex items-center">
+        <ShoppingBagOutline className="h-7 text-cyan-50" />
+      </div>
+    </header>
+  );
 }
